493ea8e9359136124568cccc6333efee9bcddb43
Fabio Germano <netcore@msn.com>
2025-03-01 09:25:13 -0300

    health check

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handle)
+	http.HandleFunc("/health", handleHealthCheck)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -18,4 +19,9 @@ func handle(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprint(w, "{\"message\":\"Hello world!\"}")
+}
+
+func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, "{\"status\":\"healthy\"}")
 }
